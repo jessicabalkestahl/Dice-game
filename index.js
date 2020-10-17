bae6b7527f7251b2a6f4856ac29065c0251d0803
Jessica Ferreira <69851821+jessicabalkestahl@users.noreply.github.com>
2020-10-17 11:10:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let randomNumber1 = Math.ceil(Math.random()*6);
+document.querySelector("img.img1").setAttribute('src', 'images/dice' + randomNumber1 + '.png');
+let randomNumber2 = Math.ceil(Math.random()*6);
+document.querySelector("img.img2").setAttribute('src', 'images/dice' + randomNumber2 + '.png');
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
